afcb18d62c8cf9670f0c18b2593656c9856cd898
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-01 16:09:17 +0100

    Added files for Task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,6 @@
 # This script installs flask
 
-package {'/usr/bin/pip3':
-    ensure => present
-}
-
-exec {'flask':
-    command => '/usr/bin/pip3 install flask==2.1.0',
-    unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
-    path    => ['/usr/bin', '/bin'],
-    require => Package['usr/bin/pip3']
+package {'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# This manifest kills a process named killmenow
+
+exec {'killmenow':
+    command => 'pkill killmenow',
+    path    => ['/usr/bin', '/bin']
+}
