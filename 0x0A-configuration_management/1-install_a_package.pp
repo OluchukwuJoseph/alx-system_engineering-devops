f234d11d34372066a02e44d4f5623fe1f4ffe9b8
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-03 23:47:55 +0100

    Resolved issue in Task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# This script installs flask
+# Using Puppet, install flask from pip3
 package {'flask':
     ensure   => '2.1.0',
     provider => 'pip3'
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
