127b7e663b14b2c0aca68b4dd77b6fb2070a9dd6
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-03 23:18:42 +0100

    Added files for Task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # This manifest makes changes to our SSH Client config file
 
-file {'/etc/ssh/sshd_config':
+file {'/etc/ssh/ssh_config':
     ensure  => file,
     content => 'IdentityFile ~/.ssh/school\nPasswordAuthentication no\n',
     mode    => '0600'
