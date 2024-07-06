bbe4c31a1cf3231bda77978d4f593240227fdbc4
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-06 17:21:00 +0100

    Resolved issue in Task 5

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,116 +1,70 @@
-# This manifest configures our nginx web server
+#  Install Nginx web server (w/ Puppet)
 
-exec {'update apt-get':
-    command => 'sudo apt-get update',
-    path    => ['/bin', '/usr/bin']
+# Update
+exec { 'apt_update':
+    command => 'apt-get update',
+    path    => ['/usr/bin', '/sbin', '/bin']
 }
 
-package {'nginx':
-    provider        => 'apt-get',
-    install_options => ['-y']
+# Install nginx
+package { 'nginx':
+    ensure   => 'installed',
+    provider => 'apt-get',
 }
 
-file {'/var/www/html/index.html':
+# Write in index.html
+file { '/var/www/html/index.html':
     ensure  => file,
-    mode    => '0744',
-    owner   => 'root',
-    group   => 'root',
-    content => 'Hello World!'
+    content => 'Hello World!\n'
 }
 
-file {'/var/www/html/404.html':
+# Write in error-page.html
+file { '/var/www/html/404.html':
     ensure  => file,
-    mode    => '0744',
-    owner   => 'root',
-    group   => 'root',
-    content => "Ceci n'est pas une page"
+    content => "Ceci n'est pas une page\n"
 }
 
-# install file_line puppet module
+# redirection & error page
+$redirection = "
+server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-exec {'file_line':
-    command => 'puppet module install puppetlabs-stdlib',
-    unless  => 'puppet module list | grep puppetlabs-stdlib',
-    path    => ['/bin', 'usr/bin', '/opt/puppetlabs/bin']
+        root /var/www/html;
+        index index.html;
+        server_name _;
+	    location / {
+                try_files \$uri \$uri/ =404;
+        }
+
+        error_page 404 /404.html;
+        location /404.html {
+                root /var/www/html;
+                internal;
+        }
+
+        location /redirect_me {
+                return 301 https://google.com;
+        }
+
+}
+"
+
+# Handle redirection and error page
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  path    => '/etc/nginx/sites-enabled/default',
+  content => $redirection
 }
 
-file_line {'ensure server name':
-    path              => '/etc/nginx/sites-enabled/default',
-    line              => 'server_name _;'
-    match             => 'server_name _;'
-    match_for_absence => true,
+# restart nginx
+exec { 'service_restart':
+  command => 'service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 
-file_line {'ensure root':
-    path              => '/etc/nginx/sites-enabled/default',
-    line              => 'root /var/www/html;',
-    match             => 'root*',
-    match_for_absence => true,
-}
-
-file_line {'ensure index':
-    path => '/etc/nginx/sites-enabled/default',
-    line => 'index index.html';,
-    match             => 'index*',
-    match_for_absence => true,
-}
-
-file_line { 'add_location_block':
-    path => '/etc/nginx/sites-enabled/default',
-    line => 'location / {',
-}
-
-file_line { 'add_try_files_line':
-    path  => '/etc/nginx/sites-enabled/default',
-    line  => '    try_files $uri $uri/ =404;',
-    match => '    try_files \$uri \$uri/ =404;',
-}
-
-file_line { 'add_closing_bracket':
-    path => '/etc/nginx/sites-enabled/default',
-    line => '}',
-}
-
-file_line { 'add_location_redirect_block_opening':
-    path => '/etc/nginx/sites-enabled/default',
-    line => 'location /redirect {',
-}
-
-file_line { 'add_return_line':
-    path  => '/etc/nginx/sites-enabled/default',
-    line  => '    return 301 http://google.com;',
-    match => '^    return 301 http://google.com;',
-}
-
-file_line { 'add_location_redirect_block_closing':
-    path => '/etc/nginx/sites-enabled/default',
-    line => '}',
-}
-
-file_line { 'add_error_page_404':
-    path  => '/etc/nginx/sites-enabled/default',
-    line  => 'error_page 404 /404.html;',
-    match => '^error_page 404',
-}
-
-file_line { 'add_location_404_block_opening':
-    path => '/etc/nginx/sites-enabled/default',
-    line => 'location /404 {',
-}
-
-file_line { 'add_root_line':
-    path  => '/etc/nginx/sites-enabled/default',
-    line  => '    root /var/www/html;',
-    match => '^    root /var/www/html;',
-}
-
-file_line { 'add_internal_line':
-    path  => '/etc/nginx/sites-enabled/default',
-    line  => '    internal;',
-    match => '^    internal;',
-}
-
-file_line { 'add_location_404_block_closing':
-    path => '/etc/nginx/sites-enabled/default',
-    line => '}',
+# start nginx
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
