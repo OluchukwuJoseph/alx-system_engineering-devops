d8eb375600b6e7d4610fe6dc108b48fac42da258
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-13 07:16:27 +0100

    Resolved issue in Task 100

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,16 @@
 # This manifest automate the task of creating a custom HTTP header response
 
-exec {'update apt-get':
-    command => 'sudo apt-get update',
-    path    => ['/bin', '/usr/bin']
+exec {'update':
+  command => '/usr/bin/apt-get update',
 }
-
-package {'nginx':
-    ensure  => installed,
-    require => Exec['update apt-get']
+-> package {'nginx':
+  ensure => 'present',
 }
-
-file {'/var/www/html/index.html':
-    ensure  => present,
-    content => 'Hello World!'
+-> file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
 }
-
-file {'/var/www/html/404.html':
-    ensure  => present,
-    content => "Ceci n'est pas une page"
-}
-
-exec {'search and replace text':
-    command => 'sudo sed -i "s|server_name _;|server_name _;\n\tadd_header X-Served-By \$hostname;|g" /etc/nginx/sites-enabled/default',
-    path    => ['/usr/bin', '/bin']
-}
-
-service { 'nginx':
-    ensure => running,
-    enable => true
+-> exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
