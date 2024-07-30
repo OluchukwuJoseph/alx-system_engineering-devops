df0eb43257f63acbbe537ede4cbfeaf42a141736
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-07-30 06:29:26 +0100

    Added file for Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+This script querys an API and returns information
+about a User TODO list progress
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    URL = "http://jsonplaceholder.typicode.com/todos"
+    PARAMS = f"userId={user_id}"
+    response = requests.get(URL, params=PARAMS)
+
+    if response.status_code == 200:
+
+        # Get Total number of tasks
+        TOTAL_NUMBER_OF_TASKS = len(response.json())
+        n_params = f"id={user_id}"
+
+        # Get Employee Name
+        n_response = requests.get('http://jsonplaceholder.typicode.com/users',
+                                  params=n_params)
+        if n_response.status_code == 200:
+            EMPLOYEE_NAME = n_response.json()[0]["name"]
+            print(EMPLOYEE_NAME)
+        else:
+            sys.exit(1)
+
+        # Get Number of done tasks
+        NUMBER_OF_DONE_TASKS = 0
+        for item in response.json():
+            if item['completed'] is True:
+                NUMBER_OF_DONE_TASKS += 1
+
+        print("Employee {} is done with tasks({}/{}):"
+              .format(EMPLOYEE_NAME,
+                      NUMBER_OF_DONE_TASKS,
+                      TOTAL_NUMBER_OF_TASKS))
+
+        for item in response.json():
+            if item['completed'] is True:
+                print(f"\t {item['title']}")
---
+++ 0x15-api/README.md
@@ -0,0 +1,5 @@
+# 0x15 API
+Welcome to the "0x15 API" repository! This project focuses on understanding and utilizing Application Programming Interfaces (APIs). APIs are crucial for enabling communication between different software systems, allowing them to exchange data and functionality. In this project, you will learn how to interact with APIs, make HTTP requests, and handle responses.
+
+## Overview
+The "0x15 API" repository contains scripts, examples, and documentation to help you work with APIs. By exploring this repository, you will gain hands-on experience in accessing and using various APIs, understanding RESTful principles, and managing data received from API responses.
