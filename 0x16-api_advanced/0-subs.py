5895f6a90ef003f1a67b76ab56734fe804e02590
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-08-19 23:11:18 +0100

    Resolved issues in Task 0 and 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,17 @@ def number_of_subscribers(subreddit):
     This function  returns the number of subscribers for a given subreddit.
     If an invalid subreddit is given, the function should return 0.
     """
+    headers = {'User-Agent': 'My User Agent'}
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": 'Google Chrome Version 81.0.4044.129'}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        data = response.json()
-        # Extract the number of subscribers
-        return data.get('data', {}).get('subscribers', 0)
+        if response.text:
+            data = response.json()
+
+            subcribers_count = data['data']['subscribers']
+            return subcribers_count
 
     return 0
