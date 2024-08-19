bd08e07446d32389ae3f543a91e32c21b518979f
Joseph Oluchukwu <devjoseph016@gmail.com>
2024-08-19 23:00:50 +0100

    Resolved issues in Task 0 and 1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,19 +11,14 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = f"https://reddit.com/r/{subreddit}/about.json"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": 'Google Chrome Version 81.0.4044.129'}
 
     response = requests.get(url, headers=headers)
-    try:
-        # Check for valid HTTP response
-        if response.status_code == 200:
-            data = response.json()
-            # Extract the number of subscribers
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            # If the subreddit does not exist or other HTTP error, return 0
-            return 0
-    except Exception:
-        # Handle any request exceptions and return 0
-        return 0
+
+    if response.status_code == 200:
+        data = response.json()
+        # Extract the number of subscribers
+        return data.get('data', {}).get('subscribers', 0)
+
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,16 +15,13 @@ def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'Google Chrome Version 81.0.4044.129'}
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        # Check for valid HTTP response
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            if posts:
-                for post in posts:
-                    print(post.get('data', {}).get('title', 'No title'))
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    # Check for valid HTTP response
+    if response.status_code == 200:
+        data = response.json()
+        posts = data.get('data').get('children')
+        if posts:
+            for post in posts:
+                print(post.get('data').get('title'))
 
-    except Exception:
-        # Handle any request exceptions and print None
-        print(None)
+    return None
